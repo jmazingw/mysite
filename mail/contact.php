99fc389b2f47236058dbe6edf007c68c279d794a
jmazingw <jdgonzales2@student.apc.edu.ph>
2023-02-07 18:43:21 +0800

    week9

--- mail/contact.php
+++ mail/contact.php
@@ -1,20 +1,39 @@
 <?php
 if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
+  echo json_encode(['message' => 'Sorry, there was a problem with your submission. Please complete the form and try again.']);
   exit();
 }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "jdgonzales2@student.apc.edu.ph"; // My Email //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "forms";
 
-if(!mail($to, $subject, $body, $header))
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
   http_response_code(500);
+  echo json_encode(['message' => 'Sorry, there was a problem with connecting to the database. Please try again later.']);
+  exit();
+}
+
+$sql = "INSERT INTO formanswers (name, subject, email, message)
+VALUES ('$name', '$subject', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+  echo json_encode(['message' => 'Your message has been sent successfully']);
+} else {
+  http_response_code(500);
+  echo json_encode(['message' => 'Sorry, there was a problem with inserting your data into the database. Please try again later.']);
+}
+
+$conn->close();
 ?>
+
